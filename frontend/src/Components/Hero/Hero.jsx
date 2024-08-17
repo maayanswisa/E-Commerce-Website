72b9bde3b132958cd0fe9e668687d231dd8c59c0
Maayan Swisa <120381527+maayanswisa@users.noreply.github.com>
2024-08-17 22:25:57 +0300

    fix

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -15,22 +15,18 @@ export const Hero = () => {
   return (
     <div className="hero">
       <div className="hero-left">
-        <h2>NEW ARRIVALS ONLY</h2>
         <div>
+          <p> </p>
+          <p> New Collections</p>
+          <p>For Everyone</p>
           <div className="hero-hand-icon">
-            <p>New</p>
             <img src={hand_icon} alt="" />
           </div>
-          <p>Collections</p>
-          <p>For Everyone</p>
         </div>
         <div className="hero-latest-btn">
           <div>Latest Collection</div>
           <img src={arrow_icon} alt="" />
         </div>
-      </div>
-      <div className="hero-right">
-        <img src={hero_image} alt="" />
       </div>
     </div>
   );
