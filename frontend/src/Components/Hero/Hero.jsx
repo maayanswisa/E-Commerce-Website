cabcd9729deef5146b015c21f821438b3634afe5
Maayan Swisa <120381527+maayanswisa@users.noreply.github.com>
2024-08-17 22:28:14 +0300

    fix

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Hero.css";
 import hand_icon from "../Assets/Frontend_Assets/hand_icon.png";
 import arrow_icon from "../Assets/Frontend_Assets/arrow.png";
-import hero_image from "../Assets/Frontend_Assets/hero_image.png";
+
 /*
 במונחים של עיצוב אתרים ושיווק, המונח 
 "Hero" 
--- frontend/src/Components/Offers/Offers.jsx
+++ frontend/src/Components/Offers/Offers.jsx
@@ -6,8 +6,8 @@ const Offers = () => {
   return (
     <div className="offers">
       <div className="offers-left">
-        <h1>Exclusive</h1>
-        <h1>Offers For You</h1>
+        <h1>Exclusive Offers </h1>
+        <h1>For You</h1>
         <p>ONLY ON BEST SELLERS PRODUCTS</p>
         <button>Check Now!</button>
       </div>
